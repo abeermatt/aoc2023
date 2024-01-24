5132d1b7fdbc48d771563913c5cd018f4cb76bdf
Matt Hobbs <matthew.hobbs@prima.it>
2024-01-24 19:58:20 +0000

    tests

--- AOC2023Tests/Day17/DijkstraTests.swift
+++ AOC2023Tests/Day17/DijkstraTests.swift
@@ -3,8 +3,19 @@ import XCTest
 final class DijkstraTests: XCTestCase {
 
     func testExample() throws {
+        let input = [
+            [ 0, 4, 0, 0, 0, 0, 0, 8, 0 ],
+            [ 4, 0, 8, 0, 0, 0, 0, 11, 0 ],
+            [ 0, 8, 0, 7, 0, 4, 0, 0, 2 ],
+            [ 0, 0, 7, 0, 9, 14, 0, 0, 0 ],
+            [ 0, 0, 0, 9, 0, 10, 0, 0, 0 ],
+            [ 0, 0, 4, 14, 10, 0, 2, 0, 0 ],
+            [ 0, 0, 0, 0, 0, 2, 0, 1, 6 ],
+            [ 8, 11, 0, 0, 0, 0, 1, 0, 7 ],
+            [ 0, 0, 2, 0, 0, 0, 6, 7, 0 ]
+            ]
         
-        var di = Dijkstra(input: [[1,2,3],[4,5,6], [7,8,9]])
+        var di = Dijkstra(input: input)
         di.start()
 //        print(result)
     }
